2eb60f6564b4795fe9a1ab8bafc775fa98151c43
ynonp <ynonperek@gmail.com>
2025-05-04 16:28:50 +0300

    add translation to question acitvity

--- app/models/activities/find_answer_activity.rb
+++ app/models/activities/find_answer_activity.rb
@@ -2,9 +2,9 @@ module Activities
   class FindAnswerActivity < Activity
     def activity_params
       {
-        text: phrases.map { |p| {id: p.id, text: p.text_l1} },
+        text: phrases.map { |p| {id: p.id, text: p.text_l1, translation: p.text_l2} },
         questions: phrases.includes(:token_translations).map do |p|
-          token_translations = p.token_translations.where("questions is not null and cardinality(questions) > 0")
+          token_translations = p.token_translations.with_questions
           {
             phrase_id: p.id,
             questions: token_translations.map do |t|
--- app/models/token_translation.rb
+++ app/models/token_translation.rb
@@ -1,3 +1,6 @@
 class TokenTranslation < ApplicationRecord
   belongs_to :phrase
+  scope :with_questions, ->() {
+    where("questions is not null and cardinality(questions) > 0")
+  }
 end
