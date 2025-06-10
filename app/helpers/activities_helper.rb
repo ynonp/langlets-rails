5a18786df031d6aa6e9e90f69cdc1e2d91a0b96d
ynonp <ynonperek@gmail.com>
2025-06-11 00:36:54 +0300

    disable audio preload in video player

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -62,7 +62,7 @@ module ActivitiesHelper
         
         if audio_url.present?
           content_tag(:div, 
-                      span_content + content_tag(:audio, "", src: audio_url, preload: "auto"),
+                      span_content + content_tag(:audio, "", src: audio_url, preload: "none"),
                       val["l2"].present? ? attributes_map : {})
         else
           span_content
