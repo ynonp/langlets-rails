319a1cc4c13abf32108efb4dc342f90e4f7a89b2
ynonp <ynonperek@gmail.com>
2025-06-18 09:08:42 +0300

    user can login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,17 +1,28 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  respond_to :html, :turbo_stream
+  layout "devise"
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super do |resource|
+      if resource.persisted?
+        respond_to do |format|
+          format.html { redirect_to after_sign_in_path_for(resource) }
+          format.turbo_stream { redirect_to after_sign_in_path_for(resource) }
+        end
+        return
+      end
+    end
+  end
+
 
   # DELETE /resource/sign_out
   # def destroy
