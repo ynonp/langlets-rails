afb694d3f7a4e2a22f73db5c74d5f1ceee6db1f8
ynonp <ynonperek@gmail.com>
2025-12-26 09:06:38 +0200

    shuffle in match phrases

--- app/models/activities/match_phrases_activity.rb
+++ app/models/activities/match_phrases_activity.rb
@@ -5,7 +5,7 @@ module Activities
     def activity_params
       {
         **video_params,
-        phrases: phrases_with_calculated_end_timestamps,
+        phrases: phrases_with_calculated_end_timestamps.shuffle,
         all_l2_phrases: lesson.medium.phrases.to_a,
         l1: phrases.first.l1,
         l2: phrases.first.l2
