a6dc9e021a2dd6094c6a380bd32441c738705888
ynonp <ynonperek@gmail.com>
2026-03-28 21:07:32 +0300

    Fix Review Tokens Flow
    1. review flashcards activities (both multi selct and write) should be
    limited to 4 tokens each
    2. write token activity should only use tokens that appeared in
    previous activities in this review lesson

--- app/services/review_lesson_builder.rb
+++ app/services/review_lesson_builder.rb
@@ -2,7 +2,8 @@ class ReviewLessonBuilder
   MIN_TOKENS_FOR_MATCH = 3
   MIN_TOKENS_FOR_CHAIN = 4
   MAX_TOKENS_PER_ACTIVITY = 15
-  MAX_TOKENS_FOR_WRITE = 10
+  MAX_TOKENS_FOR_FLASHCARD = 4
+  MAX_TOKENS_FOR_WRITE = 4
 
   def initialize(user)
     @user = user
@@ -20,27 +21,35 @@ class ReviewLessonBuilder
     )
 
     order = 1
+    previous_tokens = []
 
     if @tokens.size >= MIN_TOKENS_FOR_MATCH
       a1 = Activities::FlashcardActivity.create!(lesson: lesson, order: order, user: @user)
-      a1.token_translations = @tokens.sample([MAX_TOKENS_PER_ACTIVITY, @tokens.size].min)
+      a1_tokens = @tokens.sample([MAX_TOKENS_FOR_FLASHCARD, @tokens.size].min)
+      a1.token_translations = a1_tokens
+      previous_tokens |= a1_tokens
       order += 1
     end
 
     if @tokens.size >= MIN_TOKENS_FOR_MATCH
       a2 = Activities::MatchTokensActivity.create!(lesson: lesson, order: order, user: @user)
-      a2.token_translations = @tokens.sample([MAX_TOKENS_PER_ACTIVITY, @tokens.size].min)
+      a2_tokens = @tokens.sample([MAX_TOKENS_PER_ACTIVITY, @tokens.size].min)
+      a2.token_translations = a2_tokens
+      previous_tokens |= a2_tokens
       order += 1
     end
 
     if @tokens.size >= MIN_TOKENS_FOR_CHAIN
       a3 = Activities::TokensChainActivity.create!(lesson: lesson, order: order, user: @user)
-      a3.token_translations = @tokens.sample([MAX_TOKENS_PER_ACTIVITY, @tokens.size].min)
+      a3_tokens = @tokens.sample([MAX_TOKENS_PER_ACTIVITY, @tokens.size].min)
+      a3.token_translations = a3_tokens
+      previous_tokens |= a3_tokens
       order += 1
     end
 
+    write_pool = previous_tokens.presence || @tokens
     a4 = Activities::WriteMissingWordActivity.create!(lesson: lesson, order: order, user: @user)
-    a4.token_translations = @tokens.sample([MAX_TOKENS_FOR_WRITE, @tokens.size].min)
+    a4.token_translations = write_pool.sample([MAX_TOKENS_FOR_WRITE, write_pool.size].min)
 
     lesson
   end
