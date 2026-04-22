74fb5d8db24c884aa4aaa99877fef4cbc2ca9530
ynonp <ynonperek@gmail.com>
2026-04-22 09:14:59 +0300

    unblock mobile app login redirect

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,9 +7,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in(@user, event: :authentication)
       remember_me(@user)
       set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      redirect_to after_sign_in_path_for(@user), allow_other_host: native_app?
     else
       session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
       redirect_to new_user_registration_url
@@ -20,9 +21,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in(@user, event: :authentication)
       remember_me(@user)
       set_flash_message(:notice, :success, kind: "GitHub") if is_navigational_format?
+      redirect_to after_sign_in_path_for(@user), allow_other_host: native_app?
     else
       Rails.logger.info request.env["omniauth.auth"]
       Rails.logger.warn "User not persisted: #{@user.errors.full_messages.join(', ')}"
@@ -33,7 +35,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def failure
     if native_app?
-      redirect_to "langlets://auth-failure"
+      redirect_to "langlets://auth-failure", allow_other_host: true
     else
       redirect_to root_path
     end
