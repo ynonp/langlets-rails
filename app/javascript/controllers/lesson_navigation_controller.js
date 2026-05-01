eb59550293a30cfdda61c50ebeca126bc9391b6f
ynonp <ynonperek@gmail.com>
2026-05-01 11:58:53 +0300

    fixed navigation when finishing a course

--- app/javascript/controllers/lesson_navigation_controller.js
+++ app/javascript/controllers/lesson_navigation_controller.js
@@ -5,13 +5,20 @@ export default class extends Controller {
     lessonId: Number
   }
 
-  markLessonCompleted() {
+  markLessonCompleted(event) {
+    event.preventDefault()
     if (!this.hasLessonIdValue || !this.lessonIdValue) return
 
     const payload = JSON.stringify({ lesson_id: this.lessonIdValue })
-
     navigator.sendBeacon("/progress", new Blob([payload], {
       type: "application/json"
     }))
+
+    const href = this.element.getAttribute("href")
+    if (typeof Turbo !== "undefined") {
+      Turbo.visit(href, { action: "replace" })
+    } else {
+      window.location.href = href
+    }
   }
 }
