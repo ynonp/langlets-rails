5d06580b778432fd584b7c743bf9d9ffbd92b029
ynonp <ynonperek@gmail.com>
2026-08-30 22:38:24 +0300

    FIXED language -> phrase association

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,6 +2,5 @@ class Language < ApplicationRecord
   belongs_to :default_script, class_name: 'Script', optional: true
   has_many :courses, dependent: :destroy
   has_many :phrases_as_l1, class_name: 'Phrase', foreign_key: 'l1_id', dependent: :destroy
-  has_many :phrases_as_l2, class_name: 'Phrase', foreign_key: 'l2_id', dependent: :destroy
-
+  has_many :phrase_tokens, class_name: 'PhraseTranslation', foreign_key: 'language_id', dependent: :destroy
 end
